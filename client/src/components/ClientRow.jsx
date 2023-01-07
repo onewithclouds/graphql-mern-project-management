ec5867f72f077f3e098d6307e70145c06f3ef64c
onewithclouds <onewithclouds@protonmail.ch>
2023-01-07 23:20:32 +0200

    AddClient Mutation functionality added on the front-end

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,23 +1,23 @@
-import { FaTrash } from 'react-icons/fa';
-import { useMutation } from '@apollo/client';
-import { DELETE_CLIENT } from '../mutations/clientMutations';
-import { GET_CLIENTS } from '../queries/clientQueries';
+// import { FaTrash } from 'react-icons/fa';
+// import { useMutation } from '@apollo/client';
+// import { DELETE_CLIENT } from '../mutations/clientMutations';
+// import { GET_CLIENTS } from '../queries/clientQueries';
 import DeleteClientButton from './DeleteClientButton';
 
 export default function ClientRow( { client }) {
     
-    //updating clientRow list of clients after client delete by means of cache.readQuery-filter-cache.writeQuery, as opposed to refetchQueries
-    const [deleteClient] = useMutation(DELETE_CLIENT, {
-        variables: { id: client.id },
- //       refetchQueries: [{ query: GET_CLIENTS }],
-        update(cache, { data: { deleteClient } }) { 
-            const { clients } = cache.readQuery({ query: GET_CLIENTS });
-            cache.writeQuery({
-                query: GET_CLIENTS,
-                data: { clients: clients.filter(client => client.id !== deleteClient.id) },
-            });
-        }
-    });
+//     //updating clientRow list of clients after client delete by means of cache.readQuery-filter-cache.writeQuery, as opposed to refetchQueries
+//     const [deleteClient] = useMutation(DELETE_CLIENT, {
+//         variables: { id: client.id },
+//  //       refetchQueries: [{ query: GET_CLIENTS }],
+//         update(cache, { data: { deleteClient } }) { 
+//             const { clients } = cache.readQuery({ query: GET_CLIENTS });
+//             cache.writeQuery({
+//                 query: GET_CLIENTS,
+//                 data: { clients: clients.filter(client => client.id !== deleteClient.id) },
+//             });
+//         }
+//     });
 
   return (
     <tr>
