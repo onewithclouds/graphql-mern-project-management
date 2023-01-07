9769ffb2002ee879068be066e24f1c626bf95b3e
onewithclouds <onewithclouds@protonmail.ch>
2023-01-07 17:14:41 +0200

    Refactored DeleteClientButton into a seperate component

--- client/src/components/DeleteClientButton.jsx
+++ client/src/components/DeleteClientButton.jsx
@@ -1,11 +1,11 @@
+import { DELETE_CLIENT } from "../mutations/clientMutations"
+import { GET_CLIENTS } from "../queries/clientQueries";
 import { FaTrash } from 'react-icons/fa';
-import { useMutation } from '@apollo/client';
-import { DELETE_CLIENT } from '../mutations/clientMutations';
-import { GET_CLIENTS } from '../queries/clientQueries';
-import DeleteClientButton from './DeleteClientButton';
+import { useMutation } from "@apollo/client";
 
-export default function ClientRow( { client }) {
-    
+export default function DeleteClientButton( { client } ) {
+
+
     //updating clientRow list of clients after client delete by means of cache.readQuery-filter-cache.writeQuery, as opposed to refetchQueries
     const [deleteClient] = useMutation(DELETE_CLIENT, {
         variables: { id: client.id },
@@ -19,18 +19,10 @@ export default function ClientRow( { client }) {
         }
     });
 
+
   return (
-    <tr>
-        <td>{ client.name  }</td>
-        <td>{ client.email }</td>
-        <td>{ client.phone }</td>
-        <td>
-            <DeleteClientButton client={client} />
-            
-            {/* <button className='btn btn-danger btn-sm' onClick={deleteClient}>
-                <FaTrash />
-            </button> */}
-        </td>
-    </tr>
-  );
+    <button className='btn btn-danger btn-sm' onClick={deleteClient}>
+        <FaTrash />
+    </button>
+  )
 }
