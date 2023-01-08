631b8b5fff7ad0208ace3d70f919932ee42b05c8
onewithclouds <onewithclouds@protonmail.ch>
2023-01-08 19:25:38 +0200

    Configuring react router and Project view

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import Header from "./components/Header";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache } from  '@apollo/client';
-import Clients from './components/Clients';
-import AddClientModal from "./components/AddClientModal";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Project from "./pages/Project";
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -30,13 +32,18 @@ const client = new ApolloClient({
 function App() {
   return (
     <>
-    <ApolloProvider client={client}>
-    <Header />
-      <div className="container">
-        <AddClientModal />
-        <Clients />
-      </div>
-    </ApolloProvider>
+      <ApolloProvider client={client}>
+        <Router>
+          <Header />
+            <div className="container">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/projects/:id" element={<Project />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </div>
+        </Router>
+      </ApolloProvider>
     </>
   );
 }
